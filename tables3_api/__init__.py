ae0ef603bae493055c68530caf3c54fb6e3cbbb7
Jean Connelly <jconnelly@cfa.harvard.edu>
2017-06-05 13:06:13 -0400

    Add some more method aliases

--- tables3_api/__init__.py
+++ tables3_api/__init__.py
@@ -1,4 +1,8 @@
+import tables
+from distutils.version import LooseVersion
+
 __version__ = 0.1
+
 
 def test(*args, **kwargs):
     """
@@ -8,14 +12,20 @@ def test(*args, **kwargs):
     return testr.test(*args, **kwargs)
 
 
-import tables
-from distutils.version import LooseVersion
-
 if LooseVersion(tables.__version__) < LooseVersion('3.0'):
     # alias some methods to their pytables3 names
     tables.open_file = tables.openFile
 
     tables.table.Table.read_where = tables.table.Table.readWhere
     tables.table.Table.get_where_list = tables.table.Table.getWhereList
-    tables.table.Table.read_where = tables.table.Table.readWhere
     tables.table.Table.read_coordinates = tables.table.Table.readCoordinates
+    tables.table.Table.modify_coordinates = tables.table.Table.modifyCoordinates
+    tables.table.Table.remove_rows = tables.table.Table.removeRows
+
+    tables.table.Column.create_csindex = tables.table.Column.createCSIndex
+    tables.table.Column.create_index = tables.table.Column.createIndex
+
+    tables.file.File.get_node = tables.file.File.getNode
+    tables.file.File.create_table = tables.file.File.createTable
+    tables.file.File.create_earray = tables.file.File.createEArray
+    tables.file.File.copy_file = tables.file.File.copyFile
